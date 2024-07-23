eaf10aca33199bacb4ee7d663773603634ef4ebb
insorker <2677331029@qq.com>
2024-07-23 20:27:59 +0800

    feature: finish reading args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,85 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+  #[arg(
+    short = 'a',
+    long = "alias",
+    help = "alias names",
+  )]
+  alias: bool,
+
+  #[arg(
+    short = 'A',
+    long = "all-fqdns",
+    help = "all long host names (FQDNs)",
+  )]
+  all_fqdns: bool,
+
+  #[arg(
+    short = 'b',
+    long = "bool",
+    help = "set default hostname if none available",
+  )]
+  boot: Option<String>,
+  
+  #[arg(
+    short = 'd',
+    long = "domain",
+    help = "DNS domain name",
+  )]
+  domain: bool,
+
+  #[arg(
+    short = 'f',
+    long = "fqdn",
+    alias = "long",
+    help = "long host name (FQDN)",
+  )]
+  fqdn: bool,
+
+  #[arg(
+    short = 'F',
+    long = "file",
+    help = "read host name or NIS domain name from given file",
+  )]
+  file: Option<String>,
+
+  #[arg(
+    short = 'i',
+    long = "ip-address",
+    help = "addresses for the host name",
+  )]
+  ip_address: bool,
+
+  #[arg(
+    short = 'I',
+    long = "all-ip-address",
+    help = "all addresses for the host",
+  )]
+  all_ip_address: bool,
+
+  #[arg(
+    short = 's',
+    long = "short",
+    help = "short host name",
+  )]
+  short: bool,
+
+  #[arg(
+    short = 'y',
+    long = "yp",
+    alias = "nis",
+    help = "NIS/YP domain name",
+  )]
+  nis: bool,
+
+  hostname: Option<String>,
+}
+
 fn main() {
-  println!("Hello, world!");
+  let args = Args::parse();
+
+  print!("{:?}", args);
 }
